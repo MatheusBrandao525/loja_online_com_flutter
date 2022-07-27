baba73cc8948ab9dcc850e67b865606ab36ea527
MatheusBrandao525 <mafe123silva@gmail.com>
2022-07-26 23:03:52 -0400

    Adicionando nome ao app, animações, scroll na tela de login.

--- lib/src/auth/sign_in_screen.dart
+++ lib/src/auth/sign_in_screen.dart
@@ -1,5 +1,5 @@
+import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter/material.dart';
-import 'package:loja_online/main.dart';
 import 'package:loja_online/src/auth/components/custom_text_field.dart';
 
 class SignInScreen extends StatelessWidget {
@@ -7,137 +7,162 @@ class SignInScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
     return Scaffold(
       backgroundColor: const Color.fromARGB(255, 0, 62, 113),
-      body: Column(children: [
-        Expanded(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: const [
-              Text(
-                'TALISMÃ',
-                style: TextStyle(
-                    fontSize: 40,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.white
-                    /* color: Color.fromARGB(255, 5, 39, 67), */
-                    ),
-              ),
-              Text(
-                'Materiais p/ construçao, vidraçaria e marmoraria',
-                style: TextStyle(color: Colors.white, fontSize: 17),
-              ),
-            ],
-          ),
-        ),
-
-        // Formulario de login
-        Container(
-          padding: const EdgeInsets.symmetric(
-            horizontal: 32,
-            vertical: 40,
-          ),
-          decoration: const BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.vertical(top: Radius.circular(45))),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              // email
-              const CustomTextField(
-                icon: Icons.email,
-                label: 'Email',
-              ),
-              // senha
-              const CustomTextField(
-                icon: Icons.lock,
-                label: 'Senha',
-                isSecret: true,
-              ),
-
-              // Botao entrar
-              SizedBox(
-                height: 50,
-                child: ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(18),
-                    ),
-                  ),
-                  onPressed: () {},
-                  child: const Text(
-                    'Entrar',
+      body: SingleChildScrollView(
+        child: SizedBox(
+          height: size.height,
+          width: size.width,
+          child: Column(children: [
+            Expanded(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  // Nome do App
+                  const Text(
+                    'TALISMÃ',
                     style: TextStyle(
-                      fontSize: 18,
+                      fontSize: 40,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
                     ),
                   ),
-                ),
-              ),
 
-              // Botao esqueceu a senha
-              Align(
-                alignment: Alignment.centerRight,
-                child: TextButton(
-                  onPressed: () {},
-                  child: const Text(
-                    'Esqueceu a senha?',
-                    style: TextStyle(color: Color.fromARGB(255, 101, 116, 129)),
-                  ),
-                ),
-              ),
-
-              // Divisor
-              Padding(
-                padding: const EdgeInsets.only(bottom: 10),
-                child: Row(
-                  children: [
-                    Expanded(
-                      child: Divider(
-                        color: Colors.grey.withAlpha(90),
-                        thickness: 2,
+                  // Categorias
+                  SizedBox(
+                    height: 30,
+                    child: DefaultTextStyle(
+                      style: const TextStyle(
+                        fontSize: 19,
+                        color: Colors.white,
                       ),
-                    ),
-                    const Padding(
-                      padding: EdgeInsets.symmetric(
-                        horizontal: 15,
+                      child: AnimatedTextKit(
+                        pause: Duration.zero,
+                        repeatForever: true,
+                        animatedTexts: [
+                          FadeAnimatedText('Materiais p/ Construção'),
+                          FadeAnimatedText('Vidraçaria'),
+                          FadeAnimatedText('Marmoraria'),
+                        ],
                       ),
-                      child: Text('Ou'),
-                    ),
-                    Expanded(
-                      child: Divider(
-                        color: Colors.grey.withAlpha(90),
-                        thickness: 2,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-
-              // Botao criar conta
-              SizedBox(
-                height: 50,
-                child: OutlinedButton(
-                  style: OutlinedButton.styleFrom(
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(18),
-                      ),
-                      side: const BorderSide(
-                        width: 2,
-                        color: Colors.blue,
-                      )),
-                  onPressed: () {},
-                  child: const Text(
-                    'Criar conta',
-                    style: TextStyle(
-                      fontSize: 18,
                     ),
                   ),
-                ),
+                ],
               ),
-            ],
-          ),
+            ),
+
+            // Formulario de login
+            Container(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 32,
+                vertical: 30,
+              ),
+              decoration: const BoxDecoration(
+                  color: Colors.white,
+                  borderRadius:
+                      BorderRadius.vertical(top: Radius.circular(45))),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  // email
+                  const CustomTextField(
+                    icon: Icons.email,
+                    label: 'Email',
+                  ),
+                  // senha
+                  const CustomTextField(
+                    icon: Icons.lock,
+                    label: 'Senha',
+                    isSecret: true,
+                  ),
+
+                  // Botao entrar
+                  SizedBox(
+                    height: 50,
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(18),
+                        ),
+                      ),
+                      onPressed: () {},
+                      child: const Text(
+                        'Entrar',
+                        style: TextStyle(
+                          fontSize: 18,
+                        ),
+                      ),
+                    ),
+                  ),
+
+                  // Botao esqueceu a senha
+                  Align(
+                    alignment: Alignment.centerRight,
+                    child: TextButton(
+                      onPressed: () {},
+                      child: const Text(
+                        'Esqueceu a senha?',
+                        style: TextStyle(
+                            color: Color.fromARGB(255, 101, 116, 129)),
+                      ),
+                    ),
+                  ),
+
+                  // Divisor
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 10),
+                    child: Row(
+                      children: [
+                        Expanded(
+                          child: Divider(
+                            color: Colors.grey.withAlpha(90),
+                            thickness: 2,
+                          ),
+                        ),
+                        const Padding(
+                          padding: EdgeInsets.symmetric(
+                            horizontal: 15,
+                          ),
+                          child: Text('Ou'),
+                        ),
+                        Expanded(
+                          child: Divider(
+                            color: Colors.grey.withAlpha(90),
+                            thickness: 2,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+
+                  // Botao criar conta
+                  SizedBox(
+                    height: 50,
+                    child: OutlinedButton(
+                      style: OutlinedButton.styleFrom(
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(18),
+                          ),
+                          side: const BorderSide(
+                            width: 2,
+                            color: Colors.blue,
+                          )),
+                      onPressed: () {},
+                      child: const Text(
+                        'Criar conta',
+                        style: TextStyle(
+                          fontSize: 18,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ]),
         ),
-      ]),
+      ),
     );
   }
 }
---
+++ lib/src/config/custom_colors.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+abstract class CustomColors {
+  static Color customContrastColor = const Color.fromARGB(255, 0, 62, 113);
+}
