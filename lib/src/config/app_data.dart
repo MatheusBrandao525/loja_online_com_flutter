ef8f6faf272d65e3edb1543cf641765575730641
MatheusBrandao525 <mafe123silva@gmail.com>
2022-08-01 16:20:10 -0300

    Corrigindo erro Unable to load asset

--- lib/src/config/app_data.dart
+++ lib/src/config/app_data.dart
@@ -2,8 +2,7 @@ import 'package:loja_online/src/models/item_model.dart';
 
 ItemModel piso = ItemModel(
   description: 'Melhor piso para revestimento',
-  imgUrl:
-      '/home/matheus-brandao/projetos-flutter/loja_online/assets/Construcao/piso.jpg',
+  imgUrl: 'assets/Construcao/piso.jpg',
   itemName: 'Piso Carrara',
   price: 29.78,
   unit: 'MT',
@@ -11,8 +10,7 @@ ItemModel piso = ItemModel(
 
 ItemModel rejunte = ItemModel(
   description: 'Melhor rejunte para seu piso',
-  imgUrl:
-      '/home/matheus-brandao/projetos-flutter/loja_online/assets/Construcao/rejunte.jpg',
+  imgUrl: '/assets/Construcao/rejunte.jpg',
   itemName: 'Rejunte',
   price: 15.78,
   unit: 'KG',
@@ -20,8 +18,7 @@ ItemModel rejunte = ItemModel(
 
 ItemModel telha = ItemModel(
   description: 'A melhor telha para sua casa',
-  imgUrl:
-      '/home/matheus-brandao/projetos-flutter/loja_online/assets/Construcao/telha.jpg',
+  imgUrl: '/assets/Construcao/telha.jpg',
   itemName: 'Telha 2.44x1.10',
   price: 89.90,
   unit: 'UN',
@@ -29,8 +26,7 @@ ItemModel telha = ItemModel(
 
 ItemModel prego = ItemModel(
   description: 'Prego belgo de qualidade',
-  imgUrl:
-      '/home/matheus-brandao/projetos-flutter/loja_online/assets/Construcao/prego.jpg',
+  imgUrl: '/assets/Construcao/prego.jpg',
   itemName: 'Prego 19x36',
   price: 29.78,
   unit: 'KG',
@@ -38,8 +34,7 @@ ItemModel prego = ItemModel(
 
 ItemModel cimento = ItemModel(
   description: 'Cimento Cp II itau',
-  imgUrl:
-      '/home/matheus-brandao/projetos-flutter/loja_online/assets/Construcao/cimento.jpg',
+  imgUrl: '/assets/Construcao/cimento.jpg',
   itemName: 'Cimento',
   price: 60.00,
   unit: 'UN',
