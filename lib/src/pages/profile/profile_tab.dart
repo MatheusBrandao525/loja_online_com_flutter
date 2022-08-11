3c0541573359d16086a069efce5d9e5731582c46
MatheusBrandao525 <mafe123silva@gmail.com>
2022-08-10 22:48:06 -0400

    Dialog para atualizar senha

--- lib/src/pages/profile/profile_tab.dart
+++ lib/src/pages/profile/profile_tab.dart
@@ -3,9 +3,14 @@ import 'package:loja_online/src/config/app_data.dart' as appData;
 import 'package:flutter/material.dart';
 import 'package:loja_online/src/pages/common_widgets/custom_text_field.dart';
 
-class ProfileTab extends StatelessWidget {
+class ProfileTab extends StatefulWidget {
   const ProfileTab({Key? key}) : super(key: key);
 
+  @override
+  State<ProfileTab> createState() => _ProfileTabState();
+}
+
+class _ProfileTabState extends State<ProfileTab> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -69,7 +74,9 @@ class ProfileTab extends StatelessWidget {
                   borderRadius: BorderRadius.circular(20),
                 ),
               ),
-              onPressed: () {},
+              onPressed: () {
+                updatePassword();
+              },
               child: const Text(
                 'Atualizar senha',
                 style: TextStyle(
@@ -81,5 +88,85 @@ class ProfileTab extends StatelessWidget {
         ],
       ),
     );
+  }
+
+  Future<bool?> updatePassword() {
+    return showDialog(
+        context: context,
+        builder: (context) {
+          return Dialog(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(20),
+            ),
+            child: Stack(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(16),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      // Titulo
+                      const Padding(
+                        padding: EdgeInsets.symmetric(vertical: 12),
+                        child: Text(
+                          'Atualizar senha',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+
+                      // Senha atual
+                      const CustomTextField(
+                        icon: Icons.lock,
+                        label: 'Senha atual',
+                        isSecret: true,
+                      ),
+
+                      // Nova senha
+                      const CustomTextField(
+                        icon: Icons.lock_clock_outlined,
+                        label: 'Nova senha',
+                        isSecret: true,
+                      ),
+
+                      // Confirmar nova senha
+                      const CustomTextField(
+                        icon: Icons.lock_clock_outlined,
+                        label: 'Confirmar nova senha',
+                        isSecret: true,
+                      ),
+
+                      // Botao de confirmação
+                      SizedBox(
+                        height: 45,
+                        child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                              shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(20))),
+                          onPressed: () {},
+                          child: const Text('Atualizar'),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+                Positioned(
+                  top: 5,
+                  right: 5,
+                  child: IconButton(
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                    icon: const Icon(Icons.close),
+                  ),
+                ),
+              ],
+            ),
+          );
+        });
   }
 }
